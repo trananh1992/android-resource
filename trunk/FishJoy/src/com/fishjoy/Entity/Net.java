a245b5ed80f0a79a4575cefb6524e3fd6ca50a41
yinzch@qq.com <yinzch@qq.com@f3426d66-4770-dc98-73d9-f59aa94f800e>
2012-07-24 04:16:12 +0000

    #comment 游戏框架(Version_1)

---
+++ trunk/FishJoy/src/com/fishjoy/Entity/Net.java
@@ -0,0 +1,13 @@
+package com.fishjoy.Entity;
+
+import org.anddev.andengine.entity.sprite.AnimatedSprite;
+import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
+
+public class Net extends AnimatedSprite{
+
+	public Net(float pX, float pY, float pTileWidth, float pTileHeight,
+			TiledTextureRegion pTiledTextureRegion) {
+		super(pX, pY, pTileWidth, pTileHeight, pTiledTextureRegion);
+		// TODO Auto-generated constructor stub
+	}
+}
