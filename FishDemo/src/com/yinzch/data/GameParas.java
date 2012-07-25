ce7df60a0e19e2080c29844aa8ec0756aebfc6f7
yinzch <yinzch@qq.com>
2012-07-25 05:45:45 +0000

    #comment 提交项目FishDemo

---
+++ FishDemo/src/com/yinzch/data/GameParas.java
@@ -0,0 +1,30 @@
+package com.yinzch.data;
+
+public interface GameParas{
+	
+	public static final int CAMERA_WIDTH = 480; 
+	public static final int CAMERA_HEIGHT = 320;
+
+	public static final int LAYER_COUNT = 4;
+		
+	// 场景中各层间的次序问题
+	public static final int LAYER_BACKGROUND = 0;
+	public static final int LAYER_FISH = LAYER_BACKGROUND + 1;
+	public static final int LAYER_BULLET = LAYER_FISH;
+	public static final int LAYER_NET = LAYER_BULLET;
+	public static final int LAYER_ARTILLERY = LAYER_BULLET + 1;
+	public static final int LAYER_BUTTON = LAYER_ARTILLERY - 1;
+	public static final int LAYER_SCORE = LAYER_ARTILLERY + 1;	
+	// 一层
+	public static final int LAYER_STATE = LAYER_FISH + 1;
+	
+	public static final int ARTILLERY_MAX_RANK = 4;
+	public static final int ARTILLERY_MIN_RANK = 0;
+	
+	public static final int FISHLIST_MAX = 25;
+	
+	public int TOTAL_TIME = 60;
+	public static int MIN_SCORE = 480;
+
+}
+
---
+++ FishDemo/src/com/yinzch/fishdemo/GameActivity.java
@@ -0,0 +1,67 @@
+package com.yinzch.fishdemo;
+
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.handler.IUpdateHandler;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.Entity;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.Scene.IOnSceneTouchListener;
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.entity.sprite.TiledSprite;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.input.touch.TouchEvent;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
+import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
+import org.anddev.andengine.util.MathUtils;
+import android.util.Log;
+
+import com.yinzch.data.GameParas;
+
+public class GameActivity extends BaseGameActivity implements GameParas
+{
+	private Engine mEngine;
+	private Camera mCamera;
+	private Scene mScene;
+	
+	private TextureRegion mBackgroundTextureRegion;
+	
+	@Override
+	public Engine onLoadEngine() {
+		this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		mEngine = new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE, 
+				new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mCamera));
+		return mEngine;
+	}
+
+	@Override
+	public void onLoadResources() {
+		//TextureRegionFactory.setAssetBasePath("gfx/");
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+		BitmapTextureAtlas mBackgroundTexture = new BitmapTextureAtlas(1024, 512, 
+				TextureOptions.DEFAULT);
+		mBackgroundTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(
+				mBackgroundTexture, this, "background_easy.png", 0, 0);
+		this.mEngine.getTextureManager().loadTexture(mBackgroundTexture);
+	}
+
+	@Override
+	public Scene onLoadScene() {
+		mScene = new Scene();
+		Log.i("²ãÊý", String.valueOf(mScene.getChildCount()));
+		return null;
+	}
+
+	@Override
+	public void onLoadComplete() {
+		
+	}
+}
