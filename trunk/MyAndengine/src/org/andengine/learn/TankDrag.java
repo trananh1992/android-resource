ddc6d9e8819746dc926a6f3e72c29ea463f345ad
yinzch@qq.com <yinzch@qq.com@f3426d66-4770-dc98-73d9-f59aa94f800e>
2012-07-29 14:13:02 +0000

    #comment 修改MyEngine 【添加路径精灵；触摸监听器】

--- trunk/MyAndengine/src/org/andengine/learn/TankDrag.java
+++ trunk/MyAndengine/src/org/andengine/learn/TankDrag.java
@@ -27,7 +27,7 @@ import org.anddev.andengine.entity.scene.Scene.IOnSceneTouchListener;
 import android.util.FloatMath;
 import android.widget.Toast;
 
-public class TankActivity extends BaseGameActivity {
+public class TankDrag extends BaseGameActivity {
 
 	private static final int CAMERA_WIDTH = 800;  
     private static final int CAMERA_HEIGHT = 480;  
@@ -69,76 +69,45 @@ public class TankActivity extends BaseGameActivity {
        
         // 方法一：前两种方法都要注册
 		final AnimatedSprite face = new AnimatedSprite(100, 100, mSpriteTiledTextureRegion){
+			// 匿名子类添加新成员
+			private boolean mGrabbed;
+			
 			// 匿名子类，重写父类方法
 			@Override
 			public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, 
-					float pTouchAreaLocalY) 
+					float pTouchAreaLocalY)
 			{
 				// Innerclass通过Outerclass.this
-				TankActivity.this.runOnUiThread(new Runnable(){
-					@Override
-					public void run()
-					{
-						Toast.makeText(TankActivity.this, "Hello,face!", Toast.LENGTH_SHORT).show();
-					}
-				});
+				Toast.makeText(TankDrag.this, "Hello,face!", Toast.LENGTH_SHORT).show();
+				switch (pSceneTouchEvent.getAction()) {  
+	            case TouchEvent.ACTION_DOWN:  
+	                mGrabbed = true;  
+	                // 将原图片放大到4.5倍（之前设置的是4倍）  
+	                setScale(4.5f);  
+	                break;  
+	  
+	            case TouchEvent.ACTION_MOVE:  
+	                if (mGrabbed) {  
+	                    // 从新设置精灵的坐标  
+	                    setPosition(pSceneTouchEvent.getX(),		//通过TouchEvent实时更新坐标  
+	                            pSceneTouchEvent.getY());  
+	                }  
+	                break;  
+	  
+	            case TouchEvent.ACTION_UP:  
+	                if (mGrabbed) {  
+	                    mGrabbed = false;  
+	                    // 将图片还原到之前的样子，4倍大小  
+	                    setScale(4f);  
+	                }  
+	                break;  
+	            } 
 				return true;
 			}
 		};
 		// 注册精灵要实现触摸效果  
 	    mScene.registerTouchArea(face);
 		mScene.attachChild(face);
-		
-        // 方法2：注册触摸监听区域 
-       /* mScene.registerTouchArea(tank); 		
-        mScene.setOnAreaTouchListener(new IOnAreaTouchListener(){
-        	@Override
-        	public boolean onAreaTouched(TouchEvent pAreaTouchEvent, ITouchArea area, float t_X, float t_Y)
-        	{
-        		Toast.makeText(TankActivity.this, "Hello,tank!", Toast.LENGTH_SHORT).show();
-        		return true;
-        	}
-        });*/
-        
-        // 方法3：设置场景监听器
-        /*mScene.setOnSceneTouchListener(new IOnSceneTouchListener() // 匿名类就是一个子类，可以重写或继承父类的方法
-        {  
-        	@Override
-            public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) 
-        	{  
-
-                // 求得角度根据两个坐标，这里取一下坦克中心点作为坦克的坐标  
-                final float angleRad = MathUtils.atan2(  
-                        tank.getY() + tank.getHeight() / 2  
-                                - pSceneTouchEvent.getY(),  
-                        (tank.getX() + tank.getWidth() / 2)  
-                                - pSceneTouchEvent.getX());  
-  
-                // 求出移动的速度，这里也是根据三角形的计算法则，已知角度和总速度，求出X方向速度和Y方向速度  
-                // 100f：即为其总速度  
-                float VelocityX = FloatMath.cos(angleRad) * 100f;  
-                float VelocityY = FloatMath.sin(angleRad) * 100f;  
-  
-                switch (pSceneTouchEvent.getAction()) {  
-                // 这里，我们只取了按下抬起时的效果，方便我们观察  
-                case TouchEvent.ACTION_DOWN:  
-                case TouchEvent.ACTION_MOVE:  
-  
-                    // 为其设置X方向速度和Y方向速度  
-                    physicsHandler.setVelocity(-VelocityX, -VelocityY);  
-  
-                    // 为图片设置旋转角度(+ 90：为了修正旋转方向)  
-                    tank.setRotation(MathUtils.radToDeg(angleRad) + 90);  
-                    break;  
-                case TouchEvent.ACTION_UP:  
-                    // 松开按键后，将运动状态还原(即停止运动)  
-                    physicsHandler.reset();  
-                    break;  
-                }  
-                return true;  
-            }  
-        });*/
-        
 		return mScene;
 	}
 
@@ -148,11 +117,5 @@ public class TankActivity extends BaseGameActivity {
 		
 	}
 	
-	
-	
-	
-	
-	
-	
-    
 }
+
